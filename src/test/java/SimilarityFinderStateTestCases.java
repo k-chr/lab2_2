1c56ef48f273b56259466a0179fd780da0569338
uposledzony <chrustek123456789@gmail.com>
2020-03-24 18:29:36 +0100

    Added prototypes for test cases for state of SimilarityFinder class

--- src/test/java/SimilarityFinderStateTestCases.java
+++ src/test/java/SimilarityFinderStateTestCases.java
@@ -1,2 +1,40 @@
+import org.junit.jupiter.api.Test;
+
 public class SimilarityFinderStateTestCases {
+
+    @Test
+    void calculateJackardSimilarityForTwoEmptySetsTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForTwoDisjointSetsTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForFirstEmptySetAndSecondNotEmptyTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForFirstNotEmptySetAndSecondEmptyTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForTwoEqualNotEmptySetsTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForTwoSetsThatHaveOneElementInCommonTest(){
+
+    }
+
+    @Test
+    void calculateJackardSimilarityForTwoSetsThatDifferWithOnlyOneElementTest(){
+
+    }
+
 }

e846963ce113c93d33b4129f18d95ff54d7e1946
uposledzony <chrustek123456789@gmail.com>
2020-03-25 02:12:11 +0100

    Implemented first test case for two empty sets and init method for tests

--- src/test/java/SimilarityFinderStateTestCases.java
+++ src/test/java/SimilarityFinderStateTestCases.java
@@ -1,19 +1,33 @@
+import edu.iis.mto.search.SequenceSearcher;
 import edu.iis.mto.similarity.SimilarityFinder;
+import helpers.SequenceSearcherMock;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class SimilarityFinderStateTestCases {
 
-    private SimilarityFinder finder = null;
+    private static SimilarityFinder finder = null;
+    private static SequenceSearcher searchAlgorithm = null;
+
+    private static final double FULL_MATCHING_SETS = 1.0;
+    private static final double FULL_MISMATCHING_SETS = 0.0;
 
     @BeforeAll
     static void init(){
-
+        searchAlgorithm = new SequenceSearcherMock();
+        finder = new SimilarityFinder(searchAlgorithm);
     }
 
     @Test
     void calculateJackardSimilarityForTwoEmptySetsTest(){
+        int[] collection1 = new int[]{};
+        int[] collection2 = new int[]{};
 
+        double result = finder.calculateJackardSimilarity(collection1, collection2);
+        assertThat(result, is(FULL_MATCHING_SETS));
     }
 
     @Test

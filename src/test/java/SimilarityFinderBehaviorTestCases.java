8c899abc3fe9e4d1e45453e34c577cb2949ae793
uposledzony <chrustek123456789@gmail.com>
2020-03-25 03:25:55 +0100

    Added init method for behavior test prepared scenario.

--- src/test/java/SimilarityFinderBehaviorTestCases.java
+++ src/test/java/SimilarityFinderBehaviorTestCases.java
@@ -1,6 +1,21 @@
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.similarity.SimilarityFinder;
+import helpers.SequenceSearcherMock;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderBehaviorTestCases {
+
+    private SimilarityFinder finder = null;
+    private SequenceSearcherMock searchAlgorithm = null;
+
+    @BeforeEach
+    void init(){
+        searchAlgorithm = new SequenceSearcherMock();
+        finder = new SimilarityFinder(searchAlgorithm);
+    }
+
 
     @Test
     void checkSimilarityFinderBehaviorIfBothSequencesAreNullTest(){

a31905f1c679720ab696a5a7683787fb1a79adf2
uposledzony <chrustek123456789@gmail.com>
2020-03-25 03:09:45 +0100

    Added coverage for the case of only one matching element of two sets

--- src/test/java/SimilarityFinderStateTestCases.java
+++ src/test/java/SimilarityFinderStateTestCases.java
@@ -14,6 +14,8 @@ public class SimilarityFinderStateTestCases {
 
     private static final double FULL_MATCHING_SETS = 1.0;
     private static final double FULL_MISMATCHING_SETS = 0.0;
+    private static final double TWENTY_PERCENT = 0.2;
+    private static final double EIGHTY_PERCENT = 0.8;
 
     @BeforeAll
     static void init(){
@@ -68,7 +70,11 @@ public class SimilarityFinderStateTestCases {
 
     @Test
     void calculateJackardSimilarityForTwoSetsThatHaveOneElementInCommonTest(){
+        int[] collection1 = new int[]{11,24,313};
+        int[] collection2 = new int[]{15,24,33};
 
+        double result = finder.calculateJackardSimilarity(collection1, collection2);
+        assertThat(result, is(TWENTY_PERCENT));
     }
 
     @Test

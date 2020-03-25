40928631dbead34915c4573f3ed03da07b9dafc7
uposledzony <chrustek123456789@gmail.com>
2020-03-25 04:28:46 +0100

    Prepared two similar test cases implementations for both null and both not null sequences

--- src/test/java/SimilarityFinderBehaviorTestCases.java
+++ src/test/java/SimilarityFinderBehaviorTestCases.java
@@ -4,6 +4,9 @@ import helpers.SequenceSearcherMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SimilarityFinderBehaviorTestCases {
 
     private SimilarityFinder finder = null;
@@ -17,12 +20,20 @@ public class SimilarityFinderBehaviorTestCases {
 
     @Test
     void checkSimilarityFinderBehaviorIfBothSequencesAreNotNullTest() {
+        int[] seq1, seq2;
+        seq1 = new int[]{};
+        seq2 = new int[]{};
 
+        assertDoesNotThrow(()->finder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
     void checkSimilarityFinderBehaviorIfBothSequencesAreNullTest() {
+        int[] seq1, seq2;
+        seq1 = null;
+        seq2 = null;
 
+        assertThrows(NullPointerException.class, ()->finder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
